4879b8131c7581b62b247a6820615e55f381729a
Luc Varoqui <luc@varoqui.org>
2025-01-04 17:50:19 +0100

    prod: use new deploy workflow

--- production/update.sh
+++ production/update.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-docker compose pull showcase-website
-docker compose up -d showcase-website
+docker compose pull
+docker compose build
+docker compose up -d --remove-orphans
