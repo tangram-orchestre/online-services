ec6850509863e16a4d09a24561d8407804489dec
Luc (Echow) Varoqui <48593807+lvaroqui@users.noreply.github.com>
2025-01-12 16:27:38 +0100

    deploy: add staging (#21)

---
+++ deploy/staging/update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+docker compose pull
+docker compose build
+docker compose up -d --remove-orphans
