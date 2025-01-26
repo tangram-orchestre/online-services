d787d76ae654709cdef9e6b9bd1ca985c1f2274c
Luc Varoqui <luc@varoqui.org>
2025-01-27 00:09:50 +0100

    showcase-website: load api route from env

--- showcase-website/app.vue
+++ showcase-website/app.vue
@@ -13,14 +13,18 @@ useSeoMeta({
   ogUrl: "https://www.tangram-orchestre.fr",
   description: "Orchestre amateur pour Chef⸱fe⸱s en herbe",
   ogDescription: "Orchestre amateur pour Chef⸱fe⸱s en herbe",
-  ogImage: "http://www.tangram-orchestre.fr/images/partoches.jpg",
+  ogImage: "https://www.tangram-orchestre.fr/images/partoches.jpg",
   twitterCard: "summary_large_image",
 });
+
+const runtimeConfig = useRuntimeConfig();
 
 // configure internal service client
 client.setConfig({
   // set default base url for requests
-  baseURL: import.meta.server ? "http://backend:3000" : "https://api.localhost",
+  baseURL: import.meta.server
+    ? runtimeConfig.private_api_base_url
+    : runtimeConfig.public.api_base_url,
 });
 </script>
 
