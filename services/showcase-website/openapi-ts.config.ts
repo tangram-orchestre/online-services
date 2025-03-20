1892aa592d9937534550e0941c77d879cd1ce60c
Luc Varoqui <luc@varoqui.org>
2025-03-20 23:28:58 +0100

    portal: setup backend API generation

--- services/showcase-website/openapi-ts.config.ts
+++ services/showcase-website/openapi-ts.config.ts
@@ -2,7 +2,7 @@ import { defaultPlugins, defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
   experimentalParser: true,
-  input: "/opt/openapi/spec.json",
+  input: "/opt/openapi/public-spec.json",
   output: "client",
   plugins: [...defaultPlugins, "@hey-api/client-nuxt", "zod"],
 });
