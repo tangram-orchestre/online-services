3f0eb22dd6976eb17244422f71645265cd1393aa
Luc Varoqui <luc@varoqui.org>
2025-03-22 19:15:01 +0100

    portal: migrate to Vuetify

--- services/portal/app.vue
+++ services/portal/app.vue
@@ -2,6 +2,8 @@
 import { client } from "./client/client.gen";
 import { getUsersMe } from "~/client";
 
+import { useTheme } from "vuetify";
+
 const runtimeConfig = useRuntimeConfig();
 
 // configure internal service client
@@ -11,12 +13,13 @@ client.setConfig({
   credentials: "include",
 });
 
-const isDark = useDark({
-  selector: "html",
-  attribute: "class",
-  valueDark: "tangram-dark",
-});
+const isDark = useDark();
 const toggleDark = useToggle(isDark);
+const theme = useTheme();
+
+watch(isDark, (value) => {
+  theme.global.name.value = value ? "dark" : "light";
+});
 
 const { data, refresh } = getUsersMe({
   composable: "useAsyncData",
@@ -24,14 +27,32 @@ const { data, refresh } = getUsersMe({
 </script>
 
 <template>
-  <div>
-    <Button label="Toggle Dark Theme" icon="pi pi-moon" @click="toggleDark()" />
-    <div v-if="data">
-      Hello {{ data.first_name }} ({{ data.username }}) {{ data.last_name }}
-      {{ data.phone_number }}, how are you?
-    </div>
-    <Button label="Refresh" @click="() => refresh()" />
-  </div>
+  <v-app>
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="My Application" subtitle="Vuetify" />
+        <v-divider />
+        <v-list-item title="Navigation drawer" />
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar title="Tangram">
+      <v-btn
+        :icon="isDark ? 'mdi-weather-night' : 'mdi-brightness-5'"
+        @click="toggleDark()"
+      />
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <div v-if="data">
+          Hello {{ data.first_name }} ({{ data.username }})
+          {{ data.last_name }} {{ data.phone_number }}, how are you?
+        </div>
+        <v-btn @click="() => refresh()">Refresh</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss">
