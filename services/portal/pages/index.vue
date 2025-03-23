a3c44858dc3dcd0a33a5113d9a5b0987c06c4672
Luc Varoqui <luc@varoqui.org>
2025-03-23 16:20:29 +0100

    portal: refactor user data retrieval to use currentUser function

--- services/portal/pages/index.vue
+++ services/portal/pages/index.vue
@@ -1,69 +1,67 @@
 <script setup lang="ts">
-import { getUsersMe } from "~/client";
-
-const { data } = getUsersMe({
-  composable: "useAsyncData",
-});
+const user = await currentUser();
 </script>
 
 <template>
-  <h1>Mon profil</h1>
-  <div v-if="data" class="mt-2">
-    <h3 class="mb-2">Informations personnelles</h3>
-    <v-row>
-      <v-col cols="12" sm="6">
-        <v-text-field
-          v-model="data.username"
-          hide-details
-          label="Nom d'utilisateur"
-          readonly
-        />
-      </v-col>
-      <v-col cols="12" sm="6">
-        <v-text-field
-          v-model="data.email"
-          hide-details
-          label="Email"
-          readonly
-        />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" sm="6">
-        <v-text-field
-          v-model="data.first_name"
-          hide-details
-          label="Prénom"
-          readonly
-        />
-      </v-col>
-      <v-col>
-        <v-text-field
-          v-model="data.last_name"
-          hide-details
-          label="Nom"
-          readonly
-        />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" sm="6">
-        <v-text-field
-          v-model="data.phone_number"
-          hide-details
-          label="Téléphone"
-          readonly
-        />
-      </v-col>
-      <v-col>
-        <v-btn
-          color="primary"
-          block
-          href="https://auth.tangram-orchestre.fr/if/user/#/settings"
-        >
-          Modifier mes informations
-        </v-btn>
-      </v-col>
-    </v-row>
-  </div>
+  <v-container>
+    <h1>Mon profil</h1>
+    <div v-if="user" class="mt-2">
+      <h3 class="mb-4">Informations personnelles</h3>
+
+      <v-defaults-provider
+        :defaults="{
+          VTextField: {
+            variant: 'outlined',
+            disabled: true,
+          },
+        }"
+      >
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              v-model="user.username"
+              hide-details
+              label="Nom d'utilisateur"
+            />
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field v-model="user.email" hide-details label="Email" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              v-model="user.first_name"
+              hide-details
+              label="Prénom"
+            />
+          </v-col>
+          <v-col>
+            <v-text-field v-model="user.last_name" hide-details label="Nom" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              v-model="user.phone_number"
+              hide-details
+              label="Téléphone"
+            />
+          </v-col>
+          <v-col>
+            <v-btn
+              color="primary"
+              height="56"
+              block
+              href="https://auth.tangram-orchestre.fr/if/user/#/settings"
+            >
+              Modifier mes informations
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-defaults-provider>
+    </div>
+
+    <v-divider class="mt-4" />
+  </v-container>
 </template>
