44f43480691f7deb01d7dec1bf66c47b9967cc36
Luc Varoqui <luc@varoqui.org>
2025-05-01 23:57:33 +0200

    portal: migrate back to PrimeVue, add semesters view

--- services/backend/migrations/2025-03-30-141622_create_semesters/up.sql
+++ services/backend/migrations/2025-03-30-141622_create_semesters/up.sql
@@ -1,9 +1,9 @@
 -- Your SQL goes here
 
--- CREATE TYPE semester_period AS ENUM ('autumn', 'spring');
-
 CREATE TABLE semesters (
   id SERIAL PRIMARY KEY,
+  name VARCHAR(32) UNIQUE NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL
+  CHECK (start_date < end_date)
 );
