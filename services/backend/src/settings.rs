95b283dd921283ef21baadec67ca998275623127
Luc Varoqui <luc@varoqui.org>
2025-07-17 00:08:24 +0200

    add lgtm to dev env, send backend logs to it

--- services/backend/src/settings.rs
+++ services/backend/src/settings.rs
@@ -7,8 +7,14 @@ use config::ConfigError;
 #[derive(Debug, serde::Deserialize)]
 pub struct Settings {
     pub host: String,
+
+    pub otlp_endpoint: String,
+    pub otlp_service_name: String,
+
     pub cors_origins: String,
+
     pub altcha_secret: String,
+
     pub smtp_host: String,
     pub smtp_name: Option<String>,
     pub smtp_password: Option<String>,
